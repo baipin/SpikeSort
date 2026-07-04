67ba46d12b8713be6b7143e2c1da0b55fb90b709
baipin <bingdanapp@126.com>
2026-07-04 20:43:08 +0800

    Routine Save

---
+++ week8_kilosort4/probe00/params.py
@@ -0,0 +1,6 @@
+n_channels_dat = 385
+offset = 0
+sample_rate = 30000
+dtype = 'int16'
+hp_filtered = False
+dat_path = ['G:/ACADEMIC/ONE/cortexlab/Subjects/KS043/2020-12-07/001/raw_ephys_data/probe00/_spikeglx_ephysData_g0_t0.imec0.ap.bin']
---
+++ week8_kilosort4_runs/cde63527_probe00_20260702_011809/kilosort4/params.py
@@ -0,0 +1,6 @@
+n_channels_dat = 385
+offset = 0
+sample_rate = 30000.0
+dtype = 'int16'
+hp_filtered = False
+dat_path = ['G:/ACADEMIC/ONE/danlab/Subjects/DY_018/2020-10-20/001/raw_ephys_data/probe00/_spikeglx_ephysData_g0_t0.imec0.ap.bin']
