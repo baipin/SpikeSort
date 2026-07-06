906ec194c4bc92be51873118b1ce7a60c0534d8e
baipin <bingdanapp@126.com>
2026-07-06 08:42:14 +0800

    Soutine Save

---
+++ week8_kilosort4_runs/bb099402_probe00_20260702_202008/kilosort4/params.py
@@ -0,0 +1,6 @@
+n_channels_dat = 385
+offset = 0
+sample_rate = 30000.0
+dtype = 'int16'
+hp_filtered = False
+dat_path = ['G:/ACADEMIC/ONE/churchlandlab_ucla/Subjects/UCLA017/2021-11-16/002/raw_ephys_data/probe00/_spikeglx_ephysData_g0_t0.imec.ap.bin']
