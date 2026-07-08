4c023418f2ea7236f11db6b42b779944cb573895
baipin <bingdanapp@126.com>
2026-07-08 14:56:17 +0800

    Routine Save

---
+++ week8_kilosort4_runs/9545aa05_probe01_20260706_234838/kilosort4/params.py
@@ -0,0 +1,6 @@
+n_channels_dat = 385
+offset = 0
+sample_rate = 30000.390639481
+dtype = 'int16'
+hp_filtered = False
+dat_path = ['G:/ACADEMIC/ONE/mrsicflogellab/Subjects/SWC_058/2020-12-10/001/raw_ephys_data/probe01/_spikeglx_ephysData_g0_t0.imec1.ap.bin']
